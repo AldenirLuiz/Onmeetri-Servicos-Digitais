ebe6e24f4855da7e70637166a90c9f601dec82c1
Aldenir Luiz <aldenir.sky@gmail.com>
2025-05-28 16:59:10 -0300

    new

--- js/script-admpoint.js
+++ js/script-admpoint.js
@@ -10,7 +10,27 @@ const datafillArray = [
     "name": "Welingtom",
     "sex": "Male",
     "Age": "39"
-    }
+    },
+    {
+    "name": "Alcenir",
+    "sex": "Male",
+    "Age": "36"
+    },
+    {
+    "name": "Diogo",
+    "sex": "Male",
+    "Age": "44"
+    },
+    {
+    "name": "Joel",
+    "sex": "Male",
+    "Age": "32"
+    },
+    {
+    "name": "Joao",
+    "sex": "Male",
+    "Age": "55"
+    },
 ]
 
 document.addEventListener("DOMContentLoaded", function (){
@@ -19,6 +39,10 @@ document.addEventListener("DOMContentLoaded", function (){
     datafillArray.forEach((data, number) => {
         const newLine = `<tr class='tr-line'>
             <td>${data.name}</td>
+            <td>${data.sex}</td>
+            <td>${data.Age}</td>
+            <td style="width:'10px';"><input type="checkbox"></input></td>
+            <td style="width:'10px';"><input type='checkbox'></input></td>
         </tr>`;
         tableData.innerHTML += newLine;
     });
